9e7c66a5d004c46dab2060d1609e26a610a848ad
prakharjain1810 <prakhar.jain1810@gmail.com>
2020-03-25 11:24:47 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from math import*
+character_name="Hello World"
+character_age="67"
+print("prakhar\"jain")
+print(character_name.replace("Hello","Quiz"))
+print(39//5)
+my_num=-97
+print("number is"+str(my_num))
+print(abs(my_num))
+print(pow(my_num,2))
+print(max(4,9))
+print(min(4,9))
+print(round(3.4))
+print(sqrt(49))
+name=input("Enter your name please")
+age=input("Enter yur age please")
+print("According to your name "+name+" I can say you are sharp.And according to yor age "+age+"I can say you are tall too.")
+
